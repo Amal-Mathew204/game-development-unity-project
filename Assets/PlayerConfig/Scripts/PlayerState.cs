08faebafc6dcc6b8a255e77eaa84ea356d998764
Amal-Mathew204 <119940686+Amal-Mathew204@users.noreply.github.com>
2024-10-08 09:46:55 +0100

    Update PlayerState.cs
    Add a check for if the state the player is in is the grounded state

--- Assets/PlayerConfig/Scripts/PlayerState.cs
+++ Assets/PlayerConfig/Scripts/PlayerState.cs
@@ -7,6 +7,16 @@ namespace PlayerConfig
     public class PlayerState : MonoBehaviour
     {
         [field: SerializeField] public PlayerLocomotionState CurrentLocomotionState { get; set; } = PlayerLocomotionState.Idling;
+
+        public bool isPlayerGrounded()
+        {
+            return isStateGroundedState(CurrentLocomotionState);
+        }
+
+        public bool isStateGroundedState(PlayerLocomotionState state)
+        {
+            return state != PlayerLocomotionState.Jumping && state != PlayerLocomotionState.Falling;
+        }
     }
 
 
