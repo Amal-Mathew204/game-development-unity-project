c48b90fcff732cdaa9eb431e01195dfee70f4390
Amal-Mathew204 <119940686+Amal-Mathew204@users.noreply.github.com>
2024-10-09 15:15:01 +0100

    Change Scripts Name Space

--- Assets/Scripts/Player/PlayerState.cs
+++ Assets/Scripts/Player/PlayerState.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace PlayerConfig
+namespace Scripts.Player
 {
     public class PlayerState : MonoBehaviour
     {
