1067efbc655dcd306b4e249d279b54061df67294
Amal-Mathew204 <119940686+Amal-Mathew204@users.noreply.github.com>
2024-10-14 10:19:29 +0100

    Map Gathering Action to Input and create Event Method for the Gather Input

--- Assets/Scripts/Player/Input/PlayerLocomotionInput.cs
+++ Assets/Scripts/Player/Input/PlayerLocomotionInput.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-namespace Scripts.Player
+namespace Scripts.Player.Input
 {
     public class PlayerLocomotionInput : MonoBehaviour
     {
