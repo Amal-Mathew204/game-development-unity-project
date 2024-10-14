9be64d26015ef33dbdc7d6406609d10e5ba98caf
Amal-Mathew204 <119940686+Amal-Mathew204@users.noreply.github.com>
2024-10-14 19:34:14 +0100

    Update OptionMenuScript.cs
    Refactor Script by adding regions and naming schemes to private fields.
    
    Added Slider Fields to Script
    
    Added Slider Methods to Script
    
    Refactor of OnEnableMethod

--- Assets/Scripts/MainMenu/OptionMenuScript.cs
+++ Assets/Scripts/MainMenu/OptionMenuScript.cs
@@ -3,29 +3,67 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UIElements;
+using AudioManager = Scripts.Audio.AudioManager;
 
 namespace Scripts.MainMenu
 {
     public class OptionMenuScript : MonoBehaviour
     {
+        #region Class Variables
         [SerializeField] private UIDocument _optionMenuDocument;
         public GameObject MainMenu;
         public GameObject OptionsMenu;
-        private Button _BackButton;
+        private Button _backButton;
+        private Slider _musicSlider, _sfxSlider, _cameraSensitivitySlider;
+        #endregion
 
+        #region Enable Methods
         /// <summary>
-        /// Assigns back button and sets onclick method for it.
+        /// 
         /// </summary>
         private void OnEnable()
         {
-            VisualElement root = _optionMenuDocument.rootVisualElement;
-            _BackButton = root.Q<Button>("BackButton");
-
-            //set button clicked methods
-            _BackButton.clickable.clicked += GoBack;
-
+            GetMenuComponents();
+            SetMenuComponentEventMethods();
+            SetSliderValues();
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        private void GetMenuComponents()
+        {
+            VisualElement root = _optionMenuDocument.rootVisualElement;
+
+            // set button fields
+            _backButton = root.Q<Button>("BackButton");
+
+            //set slider fields
+            _musicSlider = root.Q<Slider>("MusicSlider");
+            _sfxSlider = root.Q<Slider>("SFXSlider");
+            _cameraSensitivitySlider = root.Q<Slider>("CameraSensitivitySlider");
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        private void SetMenuComponentEventMethods()
+        {
+            //set button clicked methods
+            _backButton.clickable.clicked += GoBack;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        private void SetSliderValues()
+        {
+            _musicSlider.value = AudioManager.Instance.musicSource.volume;
+            _sfxSlider.value = AudioManager.Instance.sfxSource.volume;
+        }
+        #endregion
+
+        #region UI Component Methods
         /// <summary>
         ///Enables main menu canvas but disables option menu canvas
         /// </summary>
@@ -35,5 +73,33 @@ namespace Scripts.MainMenu
             MainMenu.gameObject.SetActive(true);
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        public void SetSFXValue()
+        {
+            float volume = _sfxSlider.value;
+            AudioManager.Instance.sfxSource.volume = volume / 100;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public void SetMusicValue()
+        {
+            float volume = _musicSlider.value;
+            AudioManager.Instance.musicSource.volume = volume / 100;
+
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public void SetCameraSensitivity()
+        {
+            float sensitivity = _cameraSensitivitySlider.value;
+
+        }
+        #endregion
     }
 }
