dea0b84892879a8d890419c252187e8a5b5b1472
Amal-Mathew204 <119940686+Amal-Mathew204@users.noreply.github.com>
2024-10-14 10:19:29 +0100

    Map Gathering Action to Input and create Event Method for the Gather Input

---
+++ Assets/Scripts/Player/Input/PlayerActionInput.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+using System.Collections;
+
+namespace Scripts.Player.Input
+{
+    public class PlayerActionInput : MonoBehaviour
+    {
+
+        #region Class Variables
+        [field: SerializeField] public bool IsGathering { get; private set; } = false;
+        #endregion
+
+        #region Action CallBack Methods
+        public void OnGather(InputAction.CallbackContext context)
+        {
+            if (!context.performed)
+            {
+                return;
+            }
+            IsGathering = true;
+        }
+        #endregion
+    }
+}
+
