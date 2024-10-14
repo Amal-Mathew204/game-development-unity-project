5a829a13be67a1563065a19efba5ef6c61b97bb5
Amal-Mathew204 <119940686+Amal-Mathew204@users.noreply.github.com>
2024-10-14 10:24:37 +0100

    Add Player Action State (enum and field inside PlayerState)

--- Assets/Scripts/Player/PlayerState.cs
+++ Assets/Scripts/Player/PlayerState.cs
@@ -4,22 +4,34 @@ using UnityEngine;
 
 namespace Scripts.Player
 {
+    /// <summary>
+    /// Class for Storing and Changing the various States of the Player
+    /// </summary>
     public class PlayerState : MonoBehaviour
     {
         [field: SerializeField] public PlayerLocomotionState CurrentLocomotionState { get; set; } = PlayerLocomotionState.Idling;
+        [field: SerializeField] public PlayerActionState CurrentActionState { get; set; } = PlayerActionState.Idling;
 
+        /// <summary>
+        /// Checks if the Player is not airborne
+        /// </summary>
         public bool IsPlayerGrounded()
         {
             return IsStateGroundedState(CurrentLocomotionState);
         }
 
+        /// <summary>
+        /// Checks if a state is not the Jumping or Falling State
+        /// </summary>
         public bool IsStateGroundedState(PlayerLocomotionState state)
         {
             return state != PlayerLocomotionState.Jumping && state != PlayerLocomotionState.Falling;
         }
     }
 
-
+    /// <summary>
+    /// Enum for the Locomotion States of the Player
+    /// </summary>
     public enum PlayerLocomotionState
     {
         Idling = 1,
@@ -28,5 +40,14 @@ namespace Scripts.Player
         Sprinting = 4,
         Jumping = 5,
         Falling = 6
+    }
+
+    /// <summary>
+    /// Enum for the Action States of the Player
+    /// </summary>
+    public enum PlayerActionState
+    {
+        Idling = 0,
+        Gathering = 1
     }
 }
