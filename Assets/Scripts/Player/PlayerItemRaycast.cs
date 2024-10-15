783347232dd5ab2917562a10d30fb03408078e6f
Amal-Mathew204 <119940686+Amal-Mathew204@users.noreply.github.com>
2024-10-15 18:18:59 +0100

    Update PlayerItemRaycast.cs
    Add Namespace to Script

--- Assets/Scripts/Player/PlayerItemRaycast.cs
+++ Assets/Scripts/Player/PlayerItemRaycast.cs
@@ -1,53 +1,58 @@
 using UnityEngine;
+using Scripts.Item;
 
-public class PlayerItemRaycast : MonoBehaviour
+namespace Scripts.Player
 {
-    public float raycastDistance = 3f;  
-    private ItemPickup _currentItem = null;
-
-
-    /// <summary>
-    /// Create a Ray object starting from the player's position and going forward
-    /// Define RaycastHit to store information about the object the raycast hits
-    /// </summary>
-
-    void Update()
+    public class PlayerItemRaycast : MonoBehaviour
     {
-        
-        Ray ray = new Ray(transform.position, transform.forward);
-        RaycastHit hit;  
+        public float raycastDistance = 3f;  
+        private ItemPickup _currentItem = null;
 
-        // Perform the raycast and check if it hits something within the specified distance
-        if (Physics.Raycast(ray, out hit, raycastDistance))
+
+        /// <summary>
+        /// Create a Ray object starting from the player's position and going forward
+        /// Define RaycastHit to store information about the object the raycast hits
+        /// </summary>
+
+        void Update()
         {
-            // Try to get the ItemPickup component from the object the raycast hits
-            ItemPickup item = hit.collider.GetComponent<ItemPickup>();
+        
+            Ray ray = new Ray(transform.position, transform.forward);
+            RaycastHit hit;  
 
-            if (item != null)  // If we hit an item
+            // Perform the raycast and check if it hits something within the specified distance
+            if (Physics.Raycast(ray, out hit, raycastDistance))
             {
-                // If this is a new item or still the same item, handle it
-                if (item != _currentItem)
+                // Try to get the ItemPickup component from the object the raycast hits
+                ItemPickup item = hit.collider.GetComponent<ItemPickup>();
+
+                if (item != null)  // If we hit an item
                 {
-                    if (_currentItem != null)
+                    // If this is a new item or still the same item, handle it
+                    if (item != _currentItem)
                     {
-                        // If there was a previous item, call OnRaycastExit for it
-                        _currentItem.OnRaycastExit();
+                        if (_currentItem != null)
+                        {
+                            // If there was a previous item, call OnRaycastExit for it
+                            _currentItem.OnRaycastExit();
+                        }
+
+                        // Now assign the current item to the one we're pointing at
+                        _currentItem = item;
+                        _currentItem.OnRaycastHit();  
                     }
 
-                    // Now assign the current item to the one we're pointing at
-                    _currentItem = item;
-                    _currentItem.OnRaycastHit();  
+                    // Allow the player to try picking up the item if the key is pressed
+                    _currentItem.TryPickUp();
                 }
-
-                // Allow the player to try picking up the item if the key is pressed
-                _currentItem.TryPickUp();
             }
-        }
-        else if (_currentItem != null)
-        {
-            // If the raycast no longer hits an item, clear the _currentItem and call OnRaycastExit
-            _currentItem.OnRaycastExit();
-            _currentItem = null;
+            else if (_currentItem != null)
+            {
+                // If the raycast no longer hits an item, clear the _currentItem and call OnRaycastExit
+                _currentItem.OnRaycastExit();
+                _currentItem = null;
+            }
         }
     }
 }
+
