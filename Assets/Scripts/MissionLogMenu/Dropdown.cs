3e7b813a6f0a464304eb172e8910be785d63bf7c
Amal-Mathew204 <119940686+Amal-Mathew204@users.noreply.github.com>
2024-10-17 19:04:55 +0100

    Restructure Files

--- Assets/Scripts/MissionLogMenu/Dropdown.cs
+++ Assets/Scripts/MissionLogMenu/Dropdown.cs
@@ -1,8 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using Scripts.Game;
 using TMPro;
+using Scripts.Game;
+using Scripts.Quests;
+
 
 namespace Scripts.MissonLogMenu
 {
