b3b0085941fd3f208dccfaa425f4b4528f1693db
Amal-Mathew204 <119940686+Amal-Mathew204@users.noreply.github.com>
2024-10-17 09:03:47 +0100

    Add Batter Level to Game
    GameManager stores the Total Game Time in a round.
    
    This is displayed as a Battery Level on Screen
    
    If the Battery Level reaches 0% a Lose Message is displayed

--- Assets/Scripts/Game/GameState.cs
+++ Assets/Scripts/Game/GameState.cs
@@ -1,13 +1,54 @@
 ﻿using UnityEngine;
 using System.Collections;
+using TMPro;
 
 public class GameState : MonoBehaviour
 {
+    private float _gameElapsedTime = 0f;
+    private float _totalGameTime;
+    [SerializeField] private TMP_Text _batteryLevelTextField;
+    private int _batteryLevel = 100;
+
     /// <summary>
     /// Method will set Canvas Game Object in the GameStateCanvas field inside GameManager
+    /// Method will also store the total /elapsed GameTimeFrom the Game Manager
     /// </summary>
-    void Start()
+    private void Start()
     {
         GameManager.Instance.GameStateCanvas = this.gameObject.transform.Find("Canvas").gameObject;
+        _totalGameTime = GameManager.Instance.GameTime;
+        _gameElapsedTime = GameManager.Instance.GameTimeElapsed;
+
+    }
+
+    private void Update()
+    {
+        if(_batteryLevel > 0)
+        {
+            AdjustBatteryLevel();
+            if (_batteryLevel <= 0)
+            {
+                GameManager.Instance.SetPlayerHasLost();
+                SetBatteryLevel(0);
+            }
+        }
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    private void AdjustBatteryLevel()
+    {
+        _gameElapsedTime += Time.deltaTime;
+        SetBatteryLevel(Mathf.RoundToInt(((_totalGameTime - _gameElapsedTime) / _totalGameTime) * 100));
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    private void SetBatteryLevel(int level)
+    {
+        _batteryLevel = level;
+        _batteryLevelTextField.text = $"Battery Level: {_batteryLevel}%";
     }
 }
