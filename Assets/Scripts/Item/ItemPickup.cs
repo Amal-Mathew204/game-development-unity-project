3590a5de1a220af273642af7565dfbf159e6f3cf
Amal-Mathew204 <119940686+Amal-Mathew204@users.noreply.github.com>
2024-10-18 21:19:42 +0100

    Update Prefab Player > Inventory and ItemSlot

--- Assets/Scripts/Item/ItemPickup.cs
+++ Assets/Scripts/Item/ItemPickup.cs
@@ -3,7 +3,7 @@ using UnityEngine.Playables;
 using Scripts.Player;
 using Scripts.Player.Input;
 using PlayerCharacter = Scripts.Player.Player;
-
+using TMPro;
 
 namespace Scripts.Item
 {
@@ -11,12 +11,12 @@ namespace Scripts.Item
     {
         public string itemName;  
         private bool _isInRange = false;  // This will be true when the player is looking at the item
-
+        [SerializeField] private TMP_Text _itemLabel;
         private PlayerState _playerState;
         private PlayerActionInput _playerActionInput;
 
 
-        private void Start()
+        private void OnEnable()
         {
             _playerState = PlayerCharacter.Instance.gameObject.GetComponent<PlayerState>();
             _playerActionInput = PlayerCharacter.Instance.gameObject.GetComponent<PlayerActionInput>();
@@ -28,7 +28,18 @@ namespace Scripts.Item
             {
                 Debug.LogError("Player State is null");
             }
+
+            SetItemLabel();
         }
+
+        /// <summary>
+        /// Method Sets Item Label to the Name of the Item
+        /// </summary>
+        private void SetItemLabel()
+        {
+            _itemLabel.text = itemName;
+        }
+
         /// <summary>
         /// This method will be called when the raycast hits this object
         /// </summary>
