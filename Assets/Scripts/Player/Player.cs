e738d95ed8e531313c8049e3ee3ba5184e02666d
aamirnaje <aamirnaje16@gmail.com>
2024-10-18 19:43:40 +0100

    Inventory System

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -95,7 +95,7 @@ namespace Scripts.Player
         }
 
         /// <summary>
-        /// 
+        /// This is called to update the inventories UI, to ensure we are displaying the present version of items in the inventory
         /// </summary>
         private void UpdateInventoryUI()
         {
@@ -105,6 +105,7 @@ namespace Scripts.Player
                 return;
             }
 
+            // Clear previous UI elements
             inventoryText.text = "";
             foreach (GameObject button in activeButtons)
             {
@@ -118,20 +119,39 @@ namespace Scripts.Player
                 return;
             }
 
-            string itemList = "Inventory: \n";
+            // Create UI elements for each inventory item
             foreach (ItemPickup item in _inventory)
             {
-                itemList += "\t" + item.itemName + "\n";
+                // Create a parent GameObject for each item
+                GameObject itemSlot = new GameObject("ItemSlot");
+                itemSlot.transform.SetParent(_inventoryUIParent);
 
-                //inventoryText.text += item.itemName + "\n";
+                // Add RectTransform to position the elements
+                RectTransform itemSlotRect = itemSlot.AddComponent<RectTransform>();
+                itemSlotRect.sizeDelta = new Vector2(200, 200);  // Adjust size to fit both the item name and button
 
-                GameObject dropButton = Instantiate(_dropButtonPrefab, _inventoryUIParent);
-                dropButton.GetComponentInChildren<TextMeshProUGUI>().text = "Drop" + item.itemName + " \n";
+                // Create and position the item name text
+                GameObject itemTextObj = new GameObject("ItemName");
+                itemTextObj.transform.SetParent(itemSlot.transform);
+                TextMeshProUGUI itemText = itemTextObj.AddComponent<TextMeshProUGUI>();
+                itemText.text = item.itemName;
+
+                RectTransform itemTextRect = itemText.GetComponent<RectTransform>();
+                itemTextRect.sizeDelta = new Vector2(500, 30);  // Set a size for the text area
+                itemTextRect.anchoredPosition = new Vector2(0, 0);  // Position the text
+
+                // Instantiate the drop button and position it manually next to the item name
+                GameObject dropButton = Instantiate(_dropButtonPrefab, itemSlot.transform);
+                dropButton.GetComponentInChildren<TextMeshProUGUI>().text = "Drop";
                 dropButton.GetComponent<UnityEngine.UI.Button>().onClick.AddListener(() => DropItem(item));
+
+                RectTransform dropButtonRect = dropButton.GetComponent<RectTransform>();
+                dropButtonRect.sizeDelta = new Vector2(50, 30);  // Set the size for the button
+                dropButtonRect.anchoredPosition = new Vector2(160, 0);  // Adjust position to appear next to the text
+
+                // Add the button to the list of active buttons for cleanup later
                 activeButtons.Add(dropButton);
             }
-
-            inventoryText.text = itemList;
         }
         #endregion
 
@@ -164,7 +184,7 @@ namespace Scripts.Player
         }
 
         /// <summary>
-        /// 
+        /// This is called to remove items from the player's inventory
         /// </summary>
         public void RemoveItem(ItemPickup item)
         {
@@ -181,7 +201,7 @@ namespace Scripts.Player
         }
 
         /// <summary>
-        /// 
+        /// This is the function called to instantiate items from the inventory back into the world
         /// </summary>
         public void DropItem(ItemPickup item)
         {
