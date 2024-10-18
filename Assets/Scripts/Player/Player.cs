d13e91fe9437e02e230e210a8a6f24d114657351
Amal-Mathew204 <119940686+Amal-Mathew204@users.noreply.github.com>
2024-10-18 19:09:55 +0100

    Fix Item PIck Up Limit Bug

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -15,17 +15,20 @@ namespace Scripts.Player
 {
     public class Player : MonoBehaviour
     {
+        #region Class Variables
         public static Player Instance;  // Singleton instance
         private List<ItemPickup> _inventory = new List<ItemPickup>();
-
         private const int _maximumInventorySize = 10;
-
         [SerializeField] private TextMeshProUGUI inventoryText;
         [SerializeField] private GameObject _inventoryPanel;
         [SerializeField] private GameObject _dropButtonPrefab;
         [SerializeField] private Transform _inventoryUIParent;  // Parent UI object to hold inventory items
         private PlayerInventoryInput _playerInventoryInput;
         private List<GameObject> activeButtons = new List<GameObject>();
+
+        //cache values
+        private bool _isInventoryOpen = false;
+        #endregion
 
         #region Awake Methods
         private void Awake()
@@ -52,105 +55,48 @@ namespace Scripts.Player
         #endregion
 
         #region Start Methods
+        /// <summary>
+        /// Initalise Player Inventory Input Private Field
+        /// </summary>
         private void Start()
         {
             _playerInventoryInput = GetComponent<PlayerInventoryInput>();
         }
         #endregion
 
-        #region Player Item System Methods
-        /// <summary>
-        /// Add the current item to the player's inventory
-        /// </summary>
-        /// 
-
+        #region Update Methods
         private void Update()
         {
-            
-
+            ToggleInventoryUI();
         }
+        #endregion
 
+        #region Player Inventory Methods
+        /// <summary>
+        /// Method Toggles Inventory UI visability (dependant on the users input)
+        /// </summary>
         private void ToggleInventoryUI()
         {
-            if (_inventoryPanel != null)
-
+            if (_inventoryPanel == null || _playerInventoryInput.toggleInventory == _isInventoryOpen)
             {
-                bool activateInventory = _playerInventoryInput.toggleInventory;
-                _inventoryPanel.SetActive(activateInventory);
-                PlayerInput playerInput = GetComponent<PlayerInput>();
-                if (activateInventory)
-                {
-
-                    playerInput.currentActionMap.Disable();
-                    playerInput.SwitchCurrentActionMap("UI");
-                    playerInput.currentActionMap.Enable();
-                    playerInput.actions.Enable();
-                }
-                else
-                {
-
-                    playerInput.currentActionMap.Disable();
-                    playerInput.SwitchCurrentActionMap("Player");
-                    playerInput.currentActionMap.Enable();
-                    playerInput.actions.Enable();
-                }
-
-                Debug.Log(playerInput.currentActionMap.name);
-
-            }
-        }
-
-        public void AddItem(ItemPickup item)
-        {
-            
-            if (_inventory.Count >= _maximumInventorySize)
-            {
-                Debug.Log(_inventory.Count);
-                Debug.Log("Cannot add" + item.itemName + "to inventory. Inventory is full.");
                 return;
-                
+            }
+            _isInventoryOpen = !_isInventoryOpen;
+            _inventoryPanel.SetActive(_isInventoryOpen);
+            PlayerInput playerInput = GetComponent<PlayerInput>();
+            if (_isInventoryOpen)
+            {
+                playerInput.SwitchCurrentActionMap("UI");
             }
             else
             {
-                _inventory.Add(item);
-                Debug.Log(item.itemName + " added to inventory.");
-            }
-
-            Debug.Log(_inventory.Count);
-
-            HandleItemInMission(item.itemName);
-
-            UpdateInventoryUI();
-
-        }
-
-        public void RemoveItem(ItemPickup item)
-        {
-            if (_inventory.Contains(item)) {
-
-                _inventory.Remove(item);
-                Debug.Log(item.itemName + " removed from inventory.");
-                UpdateInventoryUI();
-            }
-            else {
-
-                Debug.Log("Item not found in inventory.");
+                playerInput.SwitchCurrentActionMap("Player");
             }
         }
 
-        public void DropItem(ItemPickup item)
-        {
-            RemoveItem(item);
-
-            Vector3 dropPosition = transform.position + transform.forward * 2f;
-            GameObject droppedItem = Instantiate(item.gameObject, dropPosition, Quaternion.identity);
-            droppedItem.SetActive(true);
-
-            Destroy(item);
-
-            Debug.Log(item.itemName + " dropped into the world.");  
-        }
-
+        /// <summary>
+        /// 
+        /// </summary>
         private void UpdateInventoryUI()
         {
             if (inventoryText == null)
@@ -180,15 +126,78 @@ namespace Scripts.Player
                 //inventoryText.text += item.itemName + "\n";
 
                 GameObject dropButton = Instantiate(_dropButtonPrefab, _inventoryUIParent);
-                dropButton.GetComponentInChildren<TextMeshProUGUI>().text = "Drop" + item.itemName +" \n";
+                dropButton.GetComponentInChildren<TextMeshProUGUI>().text = "Drop" + item.itemName + " \n";
                 dropButton.GetComponent<UnityEngine.UI.Button>().onClick.AddListener(() => DropItem(item));
                 activeButtons.Add(dropButton);
             }
 
             inventoryText.text = itemList;
         }
+        #endregion
 
 
+        #region Player Item Methods
+        /// <summary>
+        /// Add the current item to the player's inventory
+        /// </summary>
+        public bool AddItem(ItemPickup item)
+        {
+            
+            if (_inventory.Count >= _maximumInventorySize)
+            {
+                Debug.Log(_inventory.Count);
+                Debug.Log("Cannot add " + item.itemName + " to inventory. Inventory is full.");
+                return false;
+                
+            }
+            else
+            {
+                _inventory.Add(item);
+                Debug.Log(item.itemName + " added to inventory.");
+            }
+
+            Debug.Log(_inventory.Count);
+            HandleItemInMission(item.itemName);
+            UpdateInventoryUI();
+            return true;
+
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public void RemoveItem(ItemPickup item)
+        {
+            if (_inventory.Contains(item)) {
+
+                _inventory.Remove(item);
+                Debug.Log(item.itemName + " removed from inventory.");
+                UpdateInventoryUI();
+            }
+            else {
+
+                Debug.Log("Item not found in inventory.");
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public void DropItem(ItemPickup item)
+        {
+            RemoveItem(item);
+
+            Vector3 dropPosition = transform.position + transform.forward * 2f;
+            GameObject droppedItem = Instantiate(item.gameObject, dropPosition, Quaternion.identity);
+            droppedItem.SetActive(true);
+
+            Destroy(item);
+
+            Debug.Log(item.itemName + " dropped into the world.");  
+        }
+        #endregion
+
+        #region Mission Methods
         /// <summary>
         /// Method Checks if the Item Collected is involved in an active mission. If true the item is registered in the mission as collected.
         /// </summary>
