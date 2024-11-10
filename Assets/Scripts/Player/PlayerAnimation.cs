7e82d16bf9e6eab441094a23051a7c5207bfc20c
Amal-Mathew204 <119940686+Amal-Mathew204@users.noreply.github.com>
2024-11-10 15:23:14 +0000

    Update PlayerAnimation.cs

--- Assets/Scripts/Player/PlayerAnimation.cs
+++ Assets/Scripts/Player/PlayerAnimation.cs
@@ -14,7 +14,6 @@ namespace Scripts.Player
 
         private PlayerLocomotionInput _playerLocomotionInput;
         private PlayerState _playerState;
-        private PlayerController _playerController;
 
         //Locomotion Hashes
         private static int inputXHash = Animator.StringToHash("inputX");
@@ -23,17 +22,6 @@ namespace Scripts.Player
         private static int isGroundedHash = Animator.StringToHash("isGrounded");
         private static int isJumpingHash = Animator.StringToHash("isJumping");
         private static int isFallingHash = Animator.StringToHash("isFalling");
-        //private static int isIdlingHash = Animator.StringToHash("isIdling");
-
-        //camera rotation hashes
-        //private static int rotationMisMatchHash = Animator.StringToHash("rotationMisMatch");
-        //private static int isRotatingToTargetHash = Animator.StringToHash("isRotatingToTarget");
-
-        // Actions Hashes
-        //private static int isGatheringHash = Animator.StringToHash("isGathering");
-        //private static int isAttackingHash = Animator.StringToHash("isAttacking");
-        //private static int isPlayingActionHash = Animator.StringToHash("isPlayingAction");
-        //private int[] actionHashes;
 
 
         private Vector3 _currentBlendInput = Vector3.zero;
@@ -47,9 +35,6 @@ namespace Scripts.Player
             _playerLocomotionInput = GetComponent<PlayerLocomotionInput>();
             _playerState = GetComponent<PlayerState>();
             _playerController = GetComponent<PlayerController>();
-            //_playerActionInput = GetComponent<PlayerActionInput>();
-
-            //actionHashes = new int[] { isGatheringHash, isAttackingHash };
         }
 
         private void Update()
@@ -60,16 +45,11 @@ namespace Scripts.Player
         private void UpdateAnimationState()
         {
             // set local values of animation state
-            bool isIdling = _playerState.CurrentLocomotionState == PlayerLocomotionState.Idling;
             bool isRunning = _playerState.CurrentLocomotionState == PlayerLocomotionState.Running;
             bool isSprinting = _playerState.CurrentLocomotionState == PlayerLocomotionState.Sprinting;
             bool isJumping = _playerState.CurrentLocomotionState == PlayerLocomotionState.Jumping;
             bool isFalling = _playerState.CurrentLocomotionState == PlayerLocomotionState.Falling;
             bool isGrounded = _playerState.IsPlayerGrounded();
-            //bool isGathering = _playerState.CurrentPlayerActionState == PlayerActionState.Gathering;
-            //bool isAttacking = _playerState.CurrentPlayerActionState == PlayerActionState.Attacking;
-
-            //bool isPlayingAction = actionHashes.Any(hash => _animator.GetBool(hash));
 
             //All states have the same input magnitude for corresponding blend trees
             bool isRunBendValue = isRunning || isFalling || isJumping;
@@ -82,16 +62,10 @@ namespace Scripts.Player
             _animator.SetBool(isGroundedHash, isGrounded);
             _animator.SetBool(isFallingHash, isFalling);
             _animator.SetBool(isJumpingHash, isJumping);
-            //_animator.SetBool(isIdlingHash, isIdling);
-            //_animator.SetBool(isRotatingToTargetHash, _playerController.IsRotatingToTarget);
-            //_animator.SetBool(isAttackingHash, _playerState.CurrentPlayerActionState == PlayerActionState.Attacking);
-            //_animator.SetBool(isGatheringHash, _playerState.CurrentPlayerActionState == PlayerActionState.Gathering);
-            //_animator.SetBool(isPlayingActionHash, isPlayingAction);
 
             _animator.SetFloat(inputXHash, _currentBlendInput.x);
             _animator.SetFloat(inputYHash, _currentBlendInput.y);
             _animator.SetFloat(inputMagnitudeHash, _currentBlendInput.magnitude);
-            //_animator.SetFloat(rotationMisMatchHash, _playerController.RotationMisMatch);
 
 
         }
