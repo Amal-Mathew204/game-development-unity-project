1a0988eb900f6a0e2fecaf65ec7bc589035d3649
Amal-Mathew204 <119940686+Amal-Mathew204@users.noreply.github.com>
2024-11-11 10:19:39 +0000

    Add Disable Mouse Cursor Functionality

--- Assets/Scripts/Player/Input/PlayerInventoryInput.cs
+++ Assets/Scripts/Player/Input/PlayerInventoryInput.cs
@@ -2,15 +2,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using GameManager = Scripts.Game.GameManager;
 
 
 namespace Scripts.Player.Input
 {
     public class PlayerInventoryInput : MonoBehaviour { 
         public bool toggleInventory = false;
+        /// <summary>
+        /// Method Toggles the visibility of the Player Inventory inside the game.
+        /// The method will also toggle mouse cursor visibility dependant on the visibility of the inventory.
+        /// </summary>
         public void OnToggleInventory(InputAction.CallbackContext context) {
             if (context.performed) {
                 toggleInventory = !toggleInventory;
+            }
+
+            if (toggleInventory)
+            {
+                GameManager.Instance.EnableMouseCursor();
+            }
+            else
+            {
+                GameManager.Instance.DisableMouseCursor();
             }
 
         }
