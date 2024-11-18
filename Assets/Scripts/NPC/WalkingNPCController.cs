b7dd7766039c8cd25fa9dffde590349cdd7049a6
Amal-Mathew204 <amalmathew2004@outlook.com>
2024-11-18 09:37:41 +0000

    Add Walking NPC

---
+++ Assets/Scripts/NPC/WalkingNPCController.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// This class handles the movement and the interaction handling with the walking npc
+/// </summary>
+public class WalkingNPCController : MonoBehaviour
+{
+    #region Class Variables
+    public bool istalkingToPlayer = false;
+    #endregion
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
