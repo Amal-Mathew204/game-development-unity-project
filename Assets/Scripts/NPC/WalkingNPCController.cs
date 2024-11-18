32a3efc9976b6984bbeb3c26c19cdb5e4505f391
Amal-Mathew204 <amalmathew2004@outlook.com>
2024-11-18 10:30:32 +0000

    Add Inheritance to the WalkingNPCController Script

--- Assets/Scripts/NPC/WalkingNPCController.cs
+++ Assets/Scripts/NPC/WalkingNPCController.cs
@@ -1,24 +1,63 @@
 using System.Collections;
 using System.Collections.Generic;
+using Scripts.MissonLogMenu;
 using UnityEngine;
 
-/// <summary>
-/// This class handles the movement and the interaction handling with the walking npc
-/// </summary>
-public class WalkingNPCController : MonoBehaviour
-{
-    #region Class Variables
-    public bool istalkingToPlayer = false;
-    #endregion
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+
+namespace Scripts.NPC
+{
+    /// <summary>
+    /// This class handles the movement and the interaction handling with the walking npc
+    /// </summary>
+    public class WalkingNPCController : NPCTrigger
     {
-        
+        #region Class Variables
+        public bool istalkingToPlayer = false;
+        public List<string> NPCScript { get; set; };
+        #endregion
+
+        protected override void Start()
+        {
+            base.Start();
+            NPCScript = new List<string>();
+        }
+
+        #region TriggerBox Methods
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="other"></param>
+        protected override void OnTriggerEnter(Collider other)
+        {
+            if (other.CompareTag("Player"))
+            {
+                bubbleText.SetActive(true);
+                
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        protected override void OnTriggerExit(Collider other)
+        {
+            if (other.CompareTag("Player"))
+            {
+                bubbleText.SetActive(false);
+
+            }
+        }
+        #endregion
+
+        #region NPCConversation Methods
+        /// <summary>
+        /// This method deals with having a conversation with the NPC
+        /// </summary>
+        private void HandleNPCConversation()
+        {
+
+        }
+        #endregion
     }
 }
