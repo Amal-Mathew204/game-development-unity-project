c3da7171ddfe618e302f842e715eab47f62e01ce
aamirnaje <aamirnaje16@gmail.com>
2024-11-20 11:42:22 +0000

    Perfect Functionality for Raycasting
    - We have vastly improved the raycasting features of our game
    Changes Made:
    - Raycasting now ignores terrain allowing us to interact with items better
    - The raycasting distance is now set from the editor
    - Debugs removed
    - Added summary, to OnEnable method
    -

--- Assets/Scripts/Player/PlayerItemRaycast.cs
+++ Assets/Scripts/Player/PlayerItemRaycast.cs
@@ -5,30 +5,26 @@ namespace Scripts.Player
 {
     public class PlayerItemRaycast : MonoBehaviour
     {
-        [SerializeField] private float _raycastDistance = 100f;  
-        private ItemPickup _currentItem = null;
+        [SerializeField] private float _raycastDistance = 0f;
         [SerializeField] private LayerMask _itemLayerMask;
-
+        private ItemPickup _currentItem = null;
         /// <summary>
         /// Create a Ray object starting from the player's position and going forward
         /// Define RaycastHit to store information about the object the raycast hits
+        /// Constantly called by the update method
         /// </summary>
         void Update()
-        {
-            Debug.DrawRay(transform.position, transform.forward, Color.red, _raycastDistance);
-            
+        {            
             // Create the ray going forward
             Ray forwardRay = new Ray(transform.position, transform.forward);
-            
-
             RaycastHit hit;
+
             // Perform the raycast and check if it hits something within the specified distance
-            if (Physics.Raycast(forwardRay , out hit, _raycastDistance, _itemLayerMask))
+            if (Physics.Raycast(forwardRay , out hit, _raycastDistance, _itemLayerMask)) 
             {
                 
                 // Try to get the ItemPickup component from the object the raycast hits
                 ItemPickup item = hit.collider.GetComponent<ItemPickup>();
-
                 if (item != null)  
                 {
                     // If this is a new item or still the same item, handle it
@@ -46,7 +42,6 @@ namespace Scripts.Player
                     }
 
                     // Allow the player to try picking up the item if the key is pressed
-                    Debug.Log(item, _currentItem);
                     _currentItem.TryPickUp();
                 }
             }
