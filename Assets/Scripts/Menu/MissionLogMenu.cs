a5ed20c0274a5e5f38f943fea156e213481fed67
Aylin Turan <aylin_turanx@hotmail.com>
2024-11-29 15:32:57 +0000

    Create card for mission
    Creates cards for missions dynamically

--- Assets/Scripts/Menu/MissionLogMenu.cs
+++ Assets/Scripts/Menu/MissionLogMenu.cs
@@ -2,18 +2,33 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using PlayerManager = Scripts.Player.Player;
+using GameManager = Scripts.Game.GameManager;
+using Mission = Scripts.Quests.Mission;
 
 namespace Scripts.Menu
 {
     public class MissionLogMenu : MonoBehaviour
     {
-        /// <summary>
-        /// toggles mission log on
-        /// </summary>
-        public void MissionLog()
+        [SerializeField] private GameObject missionCardPrefab; // Drag your Mission Card prefab here in the inspector
+        [SerializeField] private Transform contentPanel; // The panel where cards will be instantiated
+
+        void Start()
         {
-            PlayerManager.Instance.ChangePauseMenuVisibility();
-            PlayerManager.Instance.ChangeMissionLogMenuVisibility();
+            GenerateMissionCards();
+        }
+
+        private void GenerateMissionCards()
+        {
+            foreach (Mission mission in GameManager.Instance.MissionList)
+            {
+                GameObject card = Instantiate(missionCardPrefab, contentPanel);
+                SetupCard(card, mission);
+            }
+        }
+
+        private void SetupCard(GameObject card, Mission mission)
+        {
+            
         }
     }
 }
