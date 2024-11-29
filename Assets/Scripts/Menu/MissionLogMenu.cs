3673ffc6b56c494d62ce1eee9c1092df2629c29d
Aylin Turan <aylin_turanx@hotmail.com>
2024-11-29 16:05:01 +0000

    Adds titles to the mission cards

--- Assets/Scripts/Menu/MissionLogMenu.cs
+++ Assets/Scripts/Menu/MissionLogMenu.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 using PlayerManager = Scripts.Player.Player;
 using GameManager = Scripts.Game.GameManager;
 using Mission = Scripts.Quests.Mission;
+using TMPro;
 
 namespace Scripts.Menu
 {
@@ -11,11 +12,18 @@ namespace Scripts.Menu
     {
         [SerializeField] private GameObject missionCardPrefab; // Drag your Mission Card prefab here in the inspector
         [SerializeField] private Transform contentPanel; // The panel where cards will be instantiated
+        public List<Mission> MissionList { get; private set; }
+        public List<string> MissionTitles { get; private set; }
+        public List<Mission> SubMissions { get; private set; } = new List<Mission>();
+
 
         void Start()
         {
             GenerateMissionCards();
+            MissionList = GameManager.Instance.MissionList;
+
         }
+
 
         private void GenerateMissionCards()
         {
@@ -28,7 +36,16 @@ namespace Scripts.Menu
 
         private void SetupCard(GameObject card, Mission mission)
         {
-            
+            TextMeshProUGUI titleText = card.GetComponentInChildren<TextMeshProUGUI>(); // Find the TextMeshPro component
+            if (titleText != null)
+            {
+                titleText.text = mission.MissionTitle; // Set the text to the mission's title
+                Debug.Log("Assigned Mission Title: " + mission.MissionTitle); // Debug to confirm it's working
+            }
+            else
+            {
+                Debug.LogError("TextMeshPro component not found on the mission card prefab!");
+            }
         }
     }
 }
