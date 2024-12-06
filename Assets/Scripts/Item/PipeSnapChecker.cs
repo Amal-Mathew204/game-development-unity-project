727d7a25ee9a4901e69b5742d509124299e8be50
Amal-Mathew204 <amalmathew2004@outlook.com>
2024-12-06 19:46:16 +0000

    Add Snapping Logic

--- Assets/Scripts/Item/PipeSnapChecker.cs
+++ Assets/Scripts/Item/PipeSnapChecker.cs
@@ -4,46 +4,91 @@ using System.Collections.Generic;
 using Scripts.Game;
 using PlayerManager = Scripts.Player.Player;
 using Unity.VisualScripting;
+using UnityEditor.TerrainTools;
 using UnityEngine;
 
 namespace Scripts.Item
 {
   public class PipeSnapChecker : MonoBehaviour
   {
-    private bool _snapperActive = false;
+    [SerializeField] private Rigidbody _itemRigidbody;
+    public bool SnapperActive = false;
     private bool _isInTriggerBox = false;
+    [SerializeField] private GameObject _snapPointOne;
+    [SerializeField] private GameObject _snapPointTwo;
+    private float _maxDistance = 10f;
+    
     private void OnTriggerEnter(Collider other)
     {
       if (other.CompareTag("Player"))
       {
         _isInTriggerBox = true;
-        if (_snapperActive == false)
+        if (SnapperActive == false)
         {
           GameScreen.Instance.ShowKeyPrompt("Confirm Set Pipe");
         }
 
       }
-      if (other.CompareTag("Pipe") && _snapperActive)
+
+      if (other.CompareTag("Pipe") && SnapperActive)
       {
-        Debug.Log("Pipe snapped");
+        PipeSnapChecker otherPipeSnapChecker = other.gameObject.GetComponentInChildren<PipeSnapChecker>();
+        if (otherPipeSnapChecker == null)
+        {
+          return;
+        }
+
+        Vector3 otherSnapPointOne = otherPipeSnapChecker._snapPointOne.transform.position;
+        Vector3 otherSnapPointTwo = otherPipeSnapChecker._snapPointTwo.transform.position;
+
+        Vector3 pipeSnapPointOne = _snapPointOne.transform.position;
+        Vector3 pipeSnapPointTwo = _snapPointTwo.transform.position;
+        
+        
+        Vector3 offset1 = pipeSnapPointOne - otherSnapPointOne;
+        Vector3 offset2 = pipeSnapPointTwo - otherSnapPointTwo;
+        Vector3 offset3 = pipeSnapPointOne - otherSnapPointTwo;
+        Vector3 offset4 = pipeSnapPointTwo - otherSnapPointOne;
+
+        bool connected = CheckOffsetSnapPoints(other, offset1) ? true :
+                         CheckOffsetSnapPoints(other, offset2) ? true : 
+                         CheckOffsetSnapPoints(other, offset3) ? true : CheckOffsetSnapPoints(other, offset4);
       }
     }
 
+    private bool CheckOffsetSnapPoints(Collider other, Vector3 offset)
+    {
+      Debug.Log("Check Offset Snap Points" + (offset.magnitude < _maxDistance));
+      if (offset.magnitude < _maxDistance)
+      {
+        MoveOtherPipe(other, offset);
+        return true;
+      }
+      return false;
+    }
+    
+    private void MoveOtherPipe(Collider other, Vector3 offset)
+    {
+      other.transform.position += offset;
+      other.GetComponent<Rigidbody>().isKinematic = true;
+    }
     private void OnDisable()
     {
-      _snapperActive = false;
+      SnapperActive = false;
       _isInTriggerBox = false;
       GameScreen.Instance.HideKeyPrompt();
+      _itemRigidbody.isKinematic = false;
     }
 
     private void Update()
     {
-      if (_isInTriggerBox && _snapperActive == false)
+      if (_isInTriggerBox && SnapperActive == false)
       {
         if (PlayerManager.Instance.getTaskAccepted())
         {
           GameScreen.Instance.HideKeyPrompt();
-          _snapperActive = true;
+          SnapperActive = true;
+          _itemRigidbody.isKinematic = true;
         }
       }
     }
