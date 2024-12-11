bab74846b2c3d9d606d084d37a15af82a85b0a13
Amal-Mathew204 <amalmathew2004@outlook.com>
2024-12-11 13:18:15 +0000

    Update LightingController.cs
    Add SunLight Rotation

--- Assets/Scripts/Lighting/LightingController.cs
+++ Assets/Scripts/Lighting/LightingController.cs
@@ -7,17 +7,25 @@ namespace Scripts.Lighting
     public class LightingController : MonoBehaviour
     {
         #region Class Variables
-        [SerializeField] private float _timeMultiplier = 2f;
+        [SerializeField] private float _timeMultiplier;
         [SerializeField] private float _startTimeHour;
+        [SerializeField] private GameObject _sunLight;
+        [SerializeField] private float _sunriseHour;
+        [SerializeField] private float _sunsetHour;
+        
         private DateTime _currentTime;
+        private TimeSpan _sunriseTime;
+        private TimeSpan _sunsetTime;
         #endregion
 
         /// <summary>
-        /// Set Initial Time for the Game
+        /// Method sets Initial Time for the Game, sunrise and sunset time
         /// </summary>
         private void Start()
         {
             _currentTime = DateTime.Now.Date + TimeSpan.FromHours(_startTimeHour);
+            _sunriseTime = TimeSpan.FromHours(_sunriseHour);
+            _sunsetTime = TimeSpan.FromHours(_sunsetHour);
         }
         /// <summary>
         /// Method changes the time of day and applies the changes to the lighting game object
@@ -26,6 +34,7 @@ namespace Scripts.Lighting
         {
             //update time in game
             UpdateTime();
+            RotateSun();
         }
 
         /// <summary>
@@ -35,6 +44,47 @@ namespace Scripts.Lighting
         {
             _currentTime = _currentTime.AddSeconds(Time.deltaTime * _timeMultiplier);
             GameScreen.Instance.UpdateTimeValue(_currentTime.ToString("HH:mm"));
+        }
+
+        /// <summary>
+        /// Method rotates the sun (lighting) game object dependent on the time in game
+        /// </summary>
+        private void RotateSun()
+        {
+            float sunRotation;
+            //day time
+            if (_currentTime.TimeOfDay > _sunriseTime && _currentTime.TimeOfDay < _sunsetTime)
+            {
+                TimeSpan sunriseToSunsetTime = GetTimeDifference(_sunriseTime, _sunsetTime);
+                TimeSpan timeFromSunrise = GetTimeDifference(_sunriseTime, _currentTime.TimeOfDay);
+                
+                double percentageTimeGone = timeFromSunrise.TotalMinutes / sunriseToSunsetTime.TotalMinutes;
+                sunRotation = Mathf.Lerp(0, 180, (float)percentageTimeGone);
+            }
+            //night time
+            else
+            {
+                TimeSpan sunsetToSunriseTime = GetTimeDifference(_sunsetTime, _sunriseTime);
+                TimeSpan timeFromSunset = GetTimeDifference(_sunsetTime, _currentTime.TimeOfDay);
+                
+                double percentageTimeGone = timeFromSunset.TotalMinutes / sunsetToSunriseTime.TotalMinutes;
+                sunRotation = Mathf.Lerp(180, 360, (float)percentageTimeGone);
+            }
+            _sunLight.transform.rotation = Quaternion.AngleAxis(sunRotation, Vector3.right);
+        }
+        
+        /// <summary>
+        /// Method returns the time difference between two time spans
+        /// </summary>
+        private TimeSpan GetTimeDifference(TimeSpan fromTime, TimeSpan toTime)
+        {
+            TimeSpan difference = toTime - fromTime;
+            //Times go over different days
+            if (difference.TotalSeconds < 0)
+            {
+                difference += TimeSpan.FromHours(24);
+            }
+            return difference;
         }
     }
 }
