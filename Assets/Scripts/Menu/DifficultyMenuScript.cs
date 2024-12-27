04877a1e375f9e95d59d1fda21c78cba78bd1584
Mohamed Zahidur Rahman <zahidur2104@gmail.com>
2024-12-27 15:52:23 +0000

    Added functionality to difficulty mode buttons

--- Assets/Scripts/Menu/DifficultyMenuScript.cs
+++ Assets/Scripts/Menu/DifficultyMenuScript.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 using UnityEngine.UIElements;
 using UnityEngine.SceneManagement;
 using GameManager = Scripts.Game.GameManager;
+using GameSettings = Scripts.Game.GameSettings;
 
 namespace Scripts.Menu
 {
@@ -13,6 +14,7 @@ namespace Scripts.Menu
         private Button _backButton, _easyButton, _mediumButton, _hardButton, _startButton;
         public GameObject MainMenu;
         public GameObject DifficultyMenu;
+        private string _selectedDifficultyMode = "Not Selected";
 
         /// <summary>
         ///Method obtains button components from the canvas difficulty menu screen and assaigns it to a variable.
@@ -21,11 +23,17 @@ namespace Scripts.Menu
         private void OnEnable()
         {
             VisualElement root = _difficultyMenuDocument.rootVisualElement;
+
+            //Set menu button fields
             _backButton = root.Q<Button>("BackButton");
             _easyButton = root.Q<Button>("EasyMode");
             _mediumButton = root.Q<Button>("MediumMode");
             _hardButton = root.Q<Button>("HardMode");
             _startButton = root.Q<Button>("StartButton");
+
+
+            //By defualt start button is disabled until user selected difficulty mode
+            _startButton.SetEnabled(false);
 
             //set button clicked methods
             _backButton.clickable.clicked += BackToMenu;
@@ -44,26 +52,43 @@ namespace Scripts.Menu
             MainMenu.gameObject.SetActive(true);
         }
 
-
+        /// <summary>
+        /// Method for selecting Easy difficulty
+        /// Also disables easy button to show user it is currently selected
+        /// </summary>
         private void SelectEasyMode()
         {
+            _selectedDifficultyMode = "Easy";
             _easyButton.SetEnabled(false);
             _mediumButton.SetEnabled(true);
             _hardButton.SetEnabled(true);
+            _startButton.SetEnabled(true);
         }
 
+        /// <summary>
+        /// Method for selecting medium difficulty
+        /// Also disables medium button to show user it is currently selected
+        /// </summary>
         private void SelectMediumMode()
         {
+            _selectedDifficultyMode = "Medium";
             _easyButton.SetEnabled(true);
             _mediumButton.SetEnabled(false);
             _hardButton.SetEnabled(true);
+            _startButton.SetEnabled(true);
         }
 
+        /// <summary>
+        /// Method for selecting hard difficulty
+        /// Also disables hard button to show user it is currently selected
+        /// </summary>
         private void SelectHardMode()
         {
+            _selectedDifficultyMode = "Hard";
             _easyButton.SetEnabled(true);
             _mediumButton.SetEnabled(true);
             _hardButton.SetEnabled(false);
+            _startButton.SetEnabled(true);
         }
 
 
@@ -72,6 +97,9 @@ namespace Scripts.Menu
         /// </summary>
         private void PlayGame()
         {
+            Debug.Log(_selectedDifficultyMode);
+            GameSettings.Instance.SetDifficultyMode(_selectedDifficultyMode);
+
             SceneManager.LoadScene("GameScene");
             GameManager.Instance.DisableMouseCursor();
         }
