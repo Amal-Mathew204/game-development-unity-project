2a10dd79fb717001ce1afebbac2422fa9650f2f8
Mohamed Zahidur Rahman <zahidur2104@gmail.com>
2024-12-30 21:47:35 +0000

    Added Save Load Method to Container

--- Assets/Scripts/Quests/Container.cs
+++ Assets/Scripts/Quests/Container.cs
@@ -4,16 +4,47 @@ using Scripts.Item;
 using System.Collections.Generic;
 using Scripts.Game;
 using Scripts.GarbageDisposal;
+using Newtonsoft.Json;
+using System;
 
 namespace Scripts.Quests
 {
     public class Container : MonoBehaviour
     {
         [SerializeField] private GarbageDisposalController _garbageDisposalController;
-        private int _count = 0;
         private int _oilDropped = 0;
-        public List<GameObject> ItemsInDisposal = new List<GameObject>();
         private bool _missionComplete = false;
+        public List<GameObject> ItemsInDisposal = new List<GameObject>();
+
+
+        #region Save/Load Methods
+
+        public void Save()
+        {
+            Dictionary<string, object> containerData = new Dictionary<string, object>();
+            containerData.Add("_missionComplete", _missionComplete);
+            containerData.Add("_oilDropped", _oilDropped);
+            PlayerPrefs.SetString("Container", JsonConvert.SerializeObject(containerData));
+        }
+
+        public void Load()
+        {
+            string dictionary = PlayerPrefs.GetString("Container");
+            Dictionary<string, object> containerData = JsonConvert.DeserializeObject<Dictionary<string, object>>(dictionary);
+            _missionComplete = (bool)containerData["_missionComplete"];
+            _oilDropped = Convert.ToInt32(containerData["_oilDropped"]);
+            GameObject[] oilBarrels = GameObject.FindGameObjectsWithTag("Barrel");
+            Debug.Log(oilBarrels.Length);
+            for (int i = 0; i < _oilDropped; i++)
+            {
+                Destroy(oilBarrels[i]);
+            }
+            if (_missionComplete)
+            {
+                _garbageDisposalController.isActive = true;
+            }
+        }
+        #endregion
 
         /// <summary>
         /// Updates the Place Barrel in Container Mission 
@@ -40,26 +71,6 @@ namespace Scripts.Quests
                 _oilDropped = _oilDropped + 1;
                 ItemsInDisposal.Add(other.gameObject);
             }
-        }
-
-        /// <summary>
-        /// Method checks for checking if barrel is in Inventory 
-        /// </summary>
-        public bool CheckBarrelInventory()
-        {
-            List<ItemPickup> inventory = PlayerManager.Instance.Inventory;
-            foreach (ItemPickup item in inventory)
-            {
-                if (item.itemName == "Barrel")
-                {
-                    _count = _count + 1;
-                }
-                if (_count == 5)
-                {
-                    return true;
-                }
-            }
-            return false;
         }
     }
 }
